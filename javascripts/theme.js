68eb30cbca1b4ddd5ba24f58b8a3c4dd93eae372
Alain Bryden <alain@analyzere.com>
2014-04-29 21:48:05 -0300

    Fix for js running before sidebar appears

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,34 +1,42 @@
-//Force the queryform to have a minimum height equal to that of the sidebar so that content below it can take up the full width.
-var head = document.getElementsByTagName('head')[0];
-var sidebarStyle = document.createElement('style');
-sidebarStyle.type = 'text/css';
-head.appendChild(sidebarStyle);
-
-function refreshQeuryFormHeight(resetHeight){
-    sidebarStyle.innerHTML = " div#query_form_with_buttons { min-height: " + (resetHeight === true ? "inherit } " : (document.getElementById('sidebar').clientHeight - 50) + "px } ");
-};
-refreshQeuryFormHeight();
-//Do this a few times at various intervals in case it takes a while for the sidebar to fill itself in.
-window.setTimeout(function(){ refreshQeuryFormHeight(); }, 100);
-window.setTimeout(function(){ refreshQeuryFormHeight(); }, 1000);
-window.setTimeout(function(){ refreshQeuryFormHeight(); }, 5000);
-
-//Make Sidebar collapsible
-var sidebar = document.getElementById('sidebar');
-var showhide = document.createElement('a');
-showhide.innerHTML = "Show/Hide"
-showhide.style.cssFloat = "right";
-showhide.style.padding = "17px";
-showhide.href = "#"
-showhide.onclick = function() {
-    sidebar.style.overflow = 'hidden';
-    if(sidebar.style.height != '30px') {
-        sidebar.style.height = '30px';
-        refreshQeuryFormHeight(true);
+function lessSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    if(sidebar == null) { //Sidebar hasn't been made yet. Try again in a decisecond
+        window.setTimeout(lessSidebar, 100);
+        return;
     }
-    else {
-        sidebar.style.height = 'inherit';
-        window.setTimeout(function(){ refreshQeuryFormHeight(); }, 100);
+    
+    //Force the queryform to have a minimum height equal to that of the sidebar so that content below it can take up the full width.
+    var head = document.getElementsByTagName('head')[0]
+    var sidebarStyle = document.createElement('style');;
+    sidebarStyle.type = 'text/css';
+    head.appendChild(sidebarStyle);
+    
+    function refreshQeuryFormHeight(resetHeight){
+        sidebarStyle.innerHTML = " div#query_form_with_buttons { min-height: " + (resetHeight === true ? "inherit } " : (sidebar.clientHeight - 50) + "px } ");
+    };
+    refreshQeuryFormHeight();
+    //Do this a few times at various intervals in case it takes a while for the sidebar to fill itself in.
+    window.setTimeout(function(){ refreshQeuryFormHeight(); }, 100);
+    window.setTimeout(function(){ refreshQeuryFormHeight(); }, 1000);
+    window.setTimeout(function(){ refreshQeuryFormHeight(); }, 5000);
+    
+    //Make Sidebar collapsible
+    var showhide = document.createElement('a');
+    showhide.innerHTML = "Show/Hide"
+    showhide.style.cssFloat = "right";
+    showhide.style.padding = "17px";
+    showhide.href = "#"
+    showhide.onclick = function() {
+        sidebar.style.overflow = 'hidden';
+        if(sidebar.style.height != '30px') {
+            sidebar.style.height = '30px';
+            refreshQeuryFormHeight(true);
+        }
+        else {
+            sidebar.style.height = 'inherit';
+            window.setTimeout(function(){ refreshQeuryFormHeight(); }, 100);
+        }
     }
+    sidebar.insertBefore(showhide, sidebar.firstChild);
 }
-sidebar.insertBefore(showhide, sidebar.firstChild);
+lessSidebar();
